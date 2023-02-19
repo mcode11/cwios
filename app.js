82ad98f9868b842548a37d3dd7b599b614633276
mcode11 <96944695+mcode11@users.noreply.github.com>
2023-02-19 17:49:08 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var term = new Terminal();
 term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-alert(term.read())
+term.prompt=()=>{
+    term.write("\r\n~ ")
+}
+term.prompt()
 function exec(str){
-comm=str.split(" ")[0]
-str=(str.slice((str.split(" ")[0].length+1),-1)+str.slice(-1)).split(" ")
-
+    comm=str.split(" ")[0]
+    str=(str.slice((str.split(" ")[0].length+1),-1)+str.slice(-1)).split(" ")
 }
