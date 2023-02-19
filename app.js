d3a173782517770cd21d8bb4b09dc265f9e7ad57
mcode11 <96944695+mcode11@users.noreply.github.com>
2023-02-19 17:45:00 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var term = new Terminal();
 term.open(document.getElementById('terminal'));
 term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+alert(term.read())
 function exec(str){
 comm=str.split(" ")[0]
 str=(str.slice((str.split(" ")[0].length+1),-1)+str.slice(-1)).split(" ")
