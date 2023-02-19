e880efe36cc15f6d2e42bd508b30a965fc1037e1
mcode11 <96944695+mcode11@users.noreply.github.com>
2023-02-19 17:05:35 +0000

    c

---
+++ app.js
@@ -0,0 +1,8 @@
+var term = new Terminal();
+term.open(document.getElementById('terminal'));
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+function exec(str){
+comm=str.split(" ")[0]
+str=(str.slice((str.split(" ")[0].length+1),-1)+str.slice(-1)).split(" ")
+
+}
